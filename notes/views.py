a3cde50cde40c599d988f18ba283e149cee92efe
unknown <parinatk1@gmail.com>
2025-05-14 10:48:07 +0700

    -Create Note Model and Add Tags to Notes

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Note
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def note_list(request):
+    notes = Note.objects.filter(owner=request.user).order_by('-updated_at')
+    return render(request, 'notes/note_list.html', {'notes': notes})
--- pkb/settings.py
+++ pkb/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',  # for full-text search later
+    'notes',
+    'taggit',  # for tagging support
 ]
 
 MIDDLEWARE = [
@@ -73,9 +77,11 @@ WSGI_APPLICATION = 'pkb.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "OPTIONS": {
+            "read_default_file": os.path.join(BASE_DIR, 'mariadb.conf'),
+        },
     }
 }
 
@@ -120,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
